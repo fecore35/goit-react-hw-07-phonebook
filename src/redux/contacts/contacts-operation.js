85001f7fcd9e6daf9d5520a4ac310ace266ba8a8
Bryzgun <bryzgun@Evgenii-Bryzgun-MacBook-Pro.local>
2021-11-21 09:25:50 +0200

    delete comments code

--- src/redux/contacts/contacts-operation.js
+++ src/redux/contacts/contacts-operation.js
@@ -12,17 +12,6 @@ export const getContactsAsync = createAsyncThunk(
   }
 );
 
-// export const getContacts = () => (dispatch) => {
-//   dispatch(actions.getContactsRequest());
-
-//   fetch("https://619416bd0b39a70017b1570d.mockapi.io/api/v1/contacts")
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch(actions.getContactsSuccesses(data));
-//     })
-//     .catch((error) => dispatch(actions.getContactsError(error)));
-// };
-
 export const addContactAsync = createAsyncThunk(
   "contacts/addContact",
   async (contact) => {
@@ -36,26 +25,6 @@ export const addContactAsync = createAsyncThunk(
   }
 );
 
-// export const addContact = (contact) => (dispatch) => {
-//   dispatch(actions.addContactRequest());
-//   console.log(contact);
-
-//   fetch("https://619416bd0b39a70017b1570d.mockapi.io/api/v1/contacts", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json;charset=utf-8",
-//     },
-//     body: JSON.stringify(contact),
-//   })
-//     .then((data) => data.json())
-//     .then((data) => {
-//       dispatch(actions.addContactSuccesses(data));
-//     })
-//     .catch((error) => {
-//       dispatch(actions.addContactError(error));
-//     });
-// };
-
 export const deleteContactAsync = createAsyncThunk(
   "contacts/deleteContact",
   async (id) => {
@@ -67,17 +36,3 @@ export const deleteContactAsync = createAsyncThunk(
     return deleteContact;
   }
 );
-
-// export const deleteContact = (id) => (dispatch) => {
-//   dispatch(actions.deleteContactRequest());
-
-//   fetch(`https://619416bd0b39a70017b1570d.mockapi.io/api/v1/contacts/${id}`, {
-//     method: "DELETE",
-//   })
-//     .then((data) => data.json())
-//     .then((data) => {
-//       console.log(data);
-//       dispatch(actions.deleteContactSuccesses(data));
-//     })
-//     .catch((error) => dispatch(actions.deleteContactError(error)));
-// };
